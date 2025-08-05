9114241f3e8c251b8321f594bfb3286e6810f70c
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-05 22:29:11 +0300

    72 Signed-In Credentials

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,5 +1,29 @@
+import 'package:flutter/services.dart';
 import 'package:oauth2/oauth2.dart';
+import 'package:re_so_fl_ttr/auth/infrastructure/credentials_storage/credentials_storage.dart';
 
 class GithubAuthenticator {
-  Future<Credentials> getSignedInCredentials() async {}
+  final CredentialsStorage _credentialsStorage;
+
+  GithubAuthenticator({required CredentialsStorage credentialsStorage})
+    : _credentialsStorage = credentialsStorage;
+
+  Future<Credentials?> getSignedInCredentials() async {
+    try {
+      final storedCredentials = await _credentialsStorage.read();
+
+      if (storedCredentials != null &&
+          storedCredentials.canRefresh &&
+          storedCredentials.isExpired) {
+        //TODO: refresh
+      }
+
+      return storedCredentials;
+    } on PlatformException {
+      return null;
+    }
+  }
+
+  Future<bool> isSignedIn() =>
+      getSignedInCredentials().then((credentials) => credentials != null);
 }
